b34b6e2eaec384924fe5582ad4097180065ca7ed
Shira Feldinger <shirafeldinger@gmail.com>
2020-08-09 12:56:36 +0300

    Updates

---
+++ precache-manifest.af9b33dc9d6f3917ebbe9b2312c02f11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c484fbfdedd01f3b68d85ab02d70d006",
+    "url": "/War-Game/index.html"
+  },
+  {
+    "revision": "1718663a457b9f585993",
+    "url": "/War-Game/static/css/main.dbb4258c.chunk.css"
+  },
+  {
+    "revision": "57547ea875e4948150f8",
+    "url": "/War-Game/static/js/2.3dfa71d9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/War-Game/static/js/2.3dfa71d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1718663a457b9f585993",
+    "url": "/War-Game/static/js/main.3a620e1d.chunk.js"
+  },
+  {
+    "revision": "6650a2d61c46cbf7987e",
+    "url": "/War-Game/static/js/runtime-main.ecae50b7.js"
+  }
+]);
